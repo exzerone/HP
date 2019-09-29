137059f33674510d1daca778be2211e8aa4fcd1c
Chris Huang <chrishuang128@gmail.com>
2019-09-28 18:38:07 -0700

    Completed skeleton of log-in page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import Frontpage from './Frontpage.jsx';
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			firstPage: true
+		};
+		this.usernameFetch = this.usernameFetch.bind(this);
 	}
+
+
 	render() {
-		return <div></div>;
+		return (
+			<div>
+				<Frontpage></Frontpage>
+			</div>
+		);
 	}
 }
---
+++ src/components/Frontpage.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+	width: 200px;
+	height: 30px;
+`;
+
+const Submit = styled.button`
+	height: 37px;
+
+	background-color: white;
+`;
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+	width: 500px;
+	height: 250px;
+	background-color: white;
+	text-align: center;
+	vertical-align: center;
+	margin: auto;
+	padding: auto;
+	position: relative;
+	border-style: solid;
+	border-width: 1px;
+`;
+
+const Greeting = styled.div`
+	font-size: 23px;
+	font-family: sans-serif;
+	font-weight: light;
+`;
+
+const InputForm = styled.form`
+	text-align: center;
+`;
+
+const Prompt = styled.label`
+	font-size: 15px;
+`;
+
+export default class Frontpage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: null
+		};
+	}
+
+	render() {
+		return (
+			<Container>
+				<Greeting>Please Log In To enter the site</Greeting>
+				<InputForm>
+					<Prompt>Enter your name: </Prompt>
+					<Input></Input>
+					<Submit type="submit">Enter</Submit>
+				</InputForm>
+			</Container>
+		);
+	}
+}
