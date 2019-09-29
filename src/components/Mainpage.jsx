e8e0694c8f49d119d9c2b2eec7eba0d0c115416d
Chris Huang <chrishuang128@gmail.com>
2019-09-29 00:34:34 -0700

    minor syntax change

---
+++ src/components/Mainpage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Navbar from './Navbar.jsx';
+
+export default class Mainpage extends Component {
+	render() {
+		return (
+			<div>
+				<Navbar />
+			</div>
+		);
+	}
+}
