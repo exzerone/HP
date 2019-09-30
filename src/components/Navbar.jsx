0e0c4c0ee396ae3b98ca62fcdfd676bb0bb104eb
Chris Huang <chrishuang128@gmail.com>
2019-09-29 20:10:16 -0700

    change backgroun color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Container = styled.div`
 	display: flex;
 	justify-content: flex-end;
 	align-items: center;
-	background-color: lightgray;
+	background-color: #fff;
 	position: relative;
 	width: 100%;
 	height: 50px;
