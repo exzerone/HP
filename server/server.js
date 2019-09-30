73a215d2ba55a958c5092ed8ba0156152983211d
Chris Huang <chrishuang128@gmail.com>
2019-09-29 18:01:23 -0700

    complete API route and query to mongo

--- server/server.js
+++ server/server.js
@@ -12,6 +12,12 @@ app.get('/', (req, res) => {
 	res.status(200).send();
 });
 
-app.get('/products', (req, res) => {});
+app.get('/products', (req, res) => {
+	Mongo.get((err, result) => {
+		if (err) console.log(err);
+		console.log('result on server', result);
+	});
+	res.status(200).send();
+});
 
 module.exports = app;
