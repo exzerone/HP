55d676bb1e847202e45c4db2b5d24f1a8502c15b
Chris Huang <chrishuang128@gmail.com>
2019-09-29 20:10:40 -0700

    change margin of product listing container

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,29 +1,54 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const Title = styled.a`
-	font-family: lato;
-	font-size: 20px;
-	letter-spacing: 0.75px;
-	text-transform: uppercase;
-	color: #000;
-	font-weight: 100;
-`;
-
 const Product = styled.div`
 	display: flex;
 	flex-direction: column;
-	width: 360px;
-	height: 715px;
+	background-color: #ffffff;
+	width: 300px;
+	height: 510px;
 	margin-top: 10px;
 	margin-bottom: 10px;
 	margin-left: 15px;
 	margin-right: 15px;
 `;
 
+const Information = styled.div`
+	display: flex;
+	padding: auto;
+	text-align: left;
+	flex-direction: column;
+	justify-content: space-evenly;
+	font-family: lato;
+	letter-spacing: 0.75px;
+	text-transform: uppercase;
+	color: #000;
+	font-weight: 100;
+`;
+
 const Image = styled.img`
 	width: 80%;
 	height: 50%;
+`;
+
+const Title = styled.a`
+	font-family: lato;
+	font-size: 20px;
+	margin-top: 20px;
+	margin-bottom: 20px;
+`;
+
+const Price = styled.div`
+	margin-top: 50px;
+	margin-bottom: 10px;
+	font-size: 25px;
+	font-weight: bold;
+`;
+
+
+const Date = styled.div`
+	margin-top: 15px;
+	font-size: 10px;
 `;
 
 export default class ProductListing extends Component {
@@ -34,14 +59,12 @@ export default class ProductListing extends Component {
 	render() {
 		return (
 			<Product>
-				{/* <Image src={this.props.data.media[0].size[3].url}></Image> */}
 				<Image src={this.props.data.media[0].sizes[3].url} alt="" />
-				<div>
+				<Information>
 					<Title>{this.props.data.title}</Title>
-					<div>{this.props.data.description}</div>
-					<div>${this.props.data.price}</div>
-					<div>{this.props.data.created_at}</div>
-				</div>
+					<Price>${this.props.data.price}</Price>
+					<Date>{this.props.data.created_at}</Date>
+				</Information>
 			</Product>
 		);
 	}
