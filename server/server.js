922e9da58b2e000e0c47af80bfa822ab3f753b3c
Chris Huang <chrishuang128@gmail.com>
2019-09-30 22:00:19 -0700

    remove unnecessary code

--- server/server.js
+++ server/server.js
@@ -15,20 +15,6 @@ app.get('/', (req, res) => {
 app.get('/products', (req, res) => {
 	Mongo.get((err, result) => {
 		if (err) console.log(err);
-		// let data = {};
-		// data.photos = [];
-		// // console.log(result[0].media[0].sizes[3].url);
-		// result.forEach((item) => {
-		// 	data.title = item.title;
-		// 	data.price = item.price;
-		// 	data.description = item.description;
-		// 	item.media.forEach((photos) => {
-		// 		if (photos.type === 'photo') {
-		// 			data.photos.push(photos.size[3].url);
-		// 		}
-		// 	});
-		// });
-
 		res.status(200).send(result);
 	});
 });
